6d6e1fc6a51a638974c3776ccabe6fe10198b056
Najwa Nisrina <najjw14a@gmail.com>
2023-10-11 07:57:01 +0700

    route
    web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\NoteController;
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,6 +22,13 @@ Route::post('/note/store', [NoteController::class, 'store'])->name('storenote');
 Route::get('/note/{id}/edit', [NoteController::class, 'edit'])->name('editnote');
 Route::put('/note/{id}', [NoteController::class, 'update'])->name('updatenote');
 Route::delete('/note/{id}', [NoteController::class, 'destroy'])->name('destroynote');
+
+// Todo
+Route::get('/todo/create', [TodoController::class, 'create'])->name('createtodo');
+Route::post('/todo/store', [TodoController::class, 'store'])->name('storetodo');
+Route::get('/todo/{id}/edit', [TodoController::class, 'edit'])->name('edittodo');
+Route::put('/todo/{id}/', [TodoController::class, 'update'])->name('updatetodo');
+Route::delete('/todo/{id}/', [TodoController::class, 'destroy'])->name('destroytodo');
 
 // Route::get('/', function () {
 //     return view('welcome');
